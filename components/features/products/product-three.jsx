ef98fa9be0513166c45de8b99b5ee9986b178046
itsolusenz <itsolusenz1@gmail.com>
2023-10-06 18:35:03 +0530

    bug fixes

--- components/features/products/product-three.jsx
+++ components/features/products/product-three.jsx
@@ -18,7 +18,9 @@ export default function ProductThree ( props ) {
         <div className={ `product-default media-with-lazy left-details product-widget ${ adClass }` }>
           
           
-          <div> <ALink href={`${product.url}`} >
+          <div style={{border:'1px solid #e0d5d5',backgroundColor:'#f1f1f1',borderRadius:'2px',padding:'10px',width:'100%'}}>
+          <div>
+             <ALink href={`${product.url}`} >
          
           <div dangerouslySetInnerHTML={{ __html: product.url}}/> 
          
@@ -26,8 +28,15 @@ export default function ProductThree ( props ) {
            
 
          </ALink> 
+         </div>
+         <h3 className="product-title">
+                    <ALink href={ "" }>&nbsp;<b>{ product.title }</b></ALink>
+                </h3>
+         <div className="product-details">
         
-         </div> 
+       </div> 
+         </div>
+         
          {/*} <h3 className="product-title">
                     <ALink href={ "" }>{ product.title }</ALink>
                 </h3>
