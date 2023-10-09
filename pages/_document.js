4e266543ff2fd89944f670b58c3a0f96b4068b37
itsolusenz <itsolusenz1@gmail.com>
2023-10-09 19:18:01 +0530

    bug fixes

--- pages/_document.js
+++ pages/_document.js
@@ -13,10 +13,10 @@ export default class MyDocument extends Document {
                     <base href="/" />
                     {process.env.NODE_ENV === "production" ? <title>Linga Chemicals</title> : ""}
                     <link rel="icon" href="favicon.png" />
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,500,600,700,800%7CPoppins:200,300,400,500,600,700,800%7COswald:300,400,600,700" />
-                    <link rel="stylesheet" type="text/css" href="vendor/bootstrap.min.css" />
-                    <link rel="stylesheet" type="text/css" href="vendor/fontawesome-free/css/all.min.css" />
-                    <link rel="stylesheet" type="text/css" href="vendor/simple-line-icons/css/simple-line-icons.min.css" />
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,500,600,700,800%7CPoppins:200,300,400,500,600,700,800%7COswald:300,400,600,700" defer />
+                    <link rel="stylesheet" type="text/css" href="vendor/bootstrap.min.css" defer />
+                    <link rel="stylesheet" type="text/css" href="vendor/fontawesome-free/css/all.min.css" defer />
+                    <link rel="stylesheet" type="text/css" href="vendor/simple-line-icons/css/simple-line-icons.min.css" defer />
                 </Head>
                 <body>
                     <Main />
